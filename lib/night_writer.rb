5932a8f3edc8f433e10fe4829520e7a6c9e7f862
andrewetobin <andrewetobin@gmail.com>
2018-07-07 13:53:18 -0600

    adds translate methods

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,19 +1,69 @@
-file = File.open(ARGV[0],"r")
+class NightWriter
+  attr_reader :line_1, :line_2, :line_3
 
-incoming_text = file.read
+  def initialize
+    @alpha_to_braille = { "a" => ['0.','..','..'],
+      "b" => ['0.','0.','..'],
+      "c" => ['00','..','..'],
+      "d" => ['00','.0','..'],
+      "e" => ['0.','.0','..'],
+      "f" => ['00','0.','..'],
+      "g" => ['00','00','..'],
+      "h" => ['0.','00','..'],
+      "i" => ['.0','0.','..'],
+      "j" => ['.0','00','..'],
+      "k" => ['0.','..','0.'],
+      "l" => ['0.','0.','0.'],
+      "m" => ['00','..','0.'],
+      "n" => ['00','.0','0.'],
+      "o" => ['0.','.0','0.'],
+      "p" => ['00','0.','0.'],
+      "q" => ['00','00','0.'],
+      "r" => ['0.','00','0.'],
+      "s" => ['.0','0.','0.'],
+      "t" => ['.0','00','0.'],
+      "u" => ['0.','..','00'],
+      "v" => ['0.','0.','00'],
+      "w" => ['.0','00','.0'],
+      "x" => ['00','..','00'],
+      "y" => ['00','.0','00'],
+      "z" => ['0.','.0','00'],
+      "!" => ['..','00','0.'],
+      "'" => ['..','..','0.'],
+      "," => ['..','0.','..'],
+      "-" => ['..','..','00'],
+      "." => ['..','00','.0'],
+      "?" => ['..','0.','00'],
+      "caps" => ['..','..','.0'],
+      " " => ['..','..','..'],
+      '#' => ['.0','.0','00']
+    }
+    @line_1 = []
+    @line_2 = []
+    @line_3 = []
+  end
 
-file.close
+  def translate(string)
+    letters = string.chars
+    arrays = letters.map do |letter|
+      @alpha_to_braille[letter]
+    end
+    format_translation(arrays)
+    # format_translation(value)
+  end
 
-capitalized_text = incoming_text.upcase
+  def format_translation(arrays)
+    formated = arrays.map do |array|
+      @line_1 << array[0]
+      @line_2 << array[1]
+      @line_3 << array[2]
+    end
 
-writer = File.open(ARGV[1],"w")
+  end
+end
 
-writer.write(capitalized_text)
-
-writer.close
-
-read_new = File.open(ARGV[1],"r")
-
-number_of_characters = read_new.read.length
-
-puts "Created '#{ARGV[1]}' containing #{number_of_characters} characters"
+night_writer = NightWriter.new
+puts night_writer.translate("he")
+puts night_writer.line_1
+puts night_writer.line_2
+puts night_writer.line_3
