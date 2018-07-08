a683aef55d8c04ce7ef7b7ee4200b159ce956a08
Tom Nunez <tomjnunez@gmail.com>
2018-07-08 12:44:12 -0600

    adds braille hash module

--- lib/file_io.rb
+++ lib/file_io.rb
@@ -1,19 +1,17 @@
-file = File.open(ARGV[0],"r")
+class FileIO
 
-incoming_text = file.read
+  def read
+    filename = ARGV[0]
+    File.read(filename)
+  end
 
-file.close
-
-capitalized_text = incoming_text.upcase
-
-writer = File.open(ARGV[1],"w")
-
-writer.write(capitalized_text)
-
-writer.close
-
-read_new = File.open(ARGV[1],"r")
-
-number_of_characters = (read_new.read.length) - 1
-
-puts "Created '#{ARGV[1]}' containing #{number_of_characters} characters"
+  def write(translation)
+    filename = ARGV[1]
+    new_braille = File.open(filename, "w")
+    new_braille.write(translation)
+    new_braille.close
+    read_new = File.open(filename, "r")
+    number_of_characters = (read_new.read.length) - 1
+    puts "Created '#{ARGV[1]}' containing #{number_of_characters} characters"
+  end
+end
